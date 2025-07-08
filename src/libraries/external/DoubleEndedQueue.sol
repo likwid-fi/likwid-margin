8fb152a39750ee89feff4ede9d1043d73d6b3930
finezoo <fighter2038@gmail.com>
2025-07-08 21:42:23 +0800

    modify

--- src/libraries/external/DoubleEndedQueue.sol
+++ src/libraries/external/DoubleEndedQueue.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v5.1.0) (utils/structs/DoubleEndedQueue.sol)
 pragma solidity ^0.8.20;
 
 import {Panic} from "@openzeppelin/contracts/utils/Panic.sol";
